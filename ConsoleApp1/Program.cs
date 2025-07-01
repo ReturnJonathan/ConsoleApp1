10f5ceaeee4534fc470584ac71caaa726590938c
DevJonth <llorestalinjn@gmail.com>
2025-07-01 12:35:26 -0500

    Primeros ajustes de mi consola

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("This is a simple console application.");
+Console.WriteLine("Press any key to exit...");
